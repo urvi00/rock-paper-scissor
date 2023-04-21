6b4af0bce94657e29984345c8159fec527d9f6ca
Urvi Gupta <urviguptareads@gmail.com>
2023-04-21 16:31:05 +0530

    Add function game to script.js

--- script.js
+++ script.js
@@ -29,6 +29,28 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    var rounds = parseInt(prompt("How many rounds would you like to play?"));
+    var userScore = 0;
+    var computerScore  = 0;
+    for (var i = 0; i < rounds; i++) {
+        userSelection = prompt("Rock, Paper or Scissor?");
+        result = playRound(userSelection, getComputerChoice());
+        alert(result);
+        if (result.substr(0,8)==="You lose") {
+            computerScore++;
+        }
+        else if (result.substr(0,8)==="You win!") {
+            userScore++;
+        }
+      }
+    if (userScore>computerScore){
+        alert(`You win this game! Your score is ${userScore}, Computer score is ${computerScore}`);
+    }
+    else if (userScore<computerScore){
+        alert(`You lose this game! Your score is ${userScore}, Computer score is ${computerScore}`);
+    }
+    else {
+        alert(`Thsi game is tied! Your score is ${userScore}, Computer score is ${computerScore}`);
+    }
+}
